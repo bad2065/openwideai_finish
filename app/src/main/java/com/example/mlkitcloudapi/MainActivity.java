82f449a762ad69c4f8cb90efb26a0bab9517bbca
nikolajirtuganov <vongu3@gmail.com>
2021-02-20 13:11:08 +0300

    MlKit Cloud Text Recognition is Working!!!

--- app/src/main/java/com/example/mlkitcloudapi/MainActivity.java
+++ app/src/main/java/com/example/mlkitcloudapi/MainActivity.java
@@ -1,16 +1,364 @@
 package com.example.mlkitcloudapi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.extensions.HdrImageCaptureExtender;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
 
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.Point;
+import android.graphics.PorterDuff;
+import android.media.Image;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.Size;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
+import com.google.firebase.ml.vision.text.FirebaseVisionCloudTextRecognizerOptions;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
+
+    private int REQUEST_CODE_PERMISSIONS = 1001;
+    private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE"};
+    private Executor executor = Executors.newSingleThreadExecutor();
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+
+    PreviewView mPreviewView;
+    SurfaceHolder holder;
+    SurfaceView surfaceView;
+    Canvas canvas;
+    Paint paint;
+    TextView textView;
+    int cameraHeight, cameraWidth, xOffset, yOffset, boxWidth, boxHeight;
+
+//    ImageView captureImage;
+//    TextView tvTextFromCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //sdjfhgsdjhfdf
-        //dkfjgh
+
+//        captureImage = findViewById(R.id.captureImg);
+//        tvTextFromCamera = findViewById(R.id.tvTextFromCamera);
+
+//        if(allPermissionsGranted()){
+            startCamera(); //start camera if permission has been granted by user
+//        } else{
+//            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
+//        }
+
+        surfaceView = findViewById(R.id.overlay);
+        surfaceView.setZOrderOnTop(true);
+        holder = surfaceView.getHolder();
+        holder.setFormat(PixelFormat.TRANSPARENT);
+        holder.addCallback(this);
+    }
+
+    private int degreesToFirebaseRotation(int degrees) {
+        switch (degrees) {
+            case 0:
+                return FirebaseVisionImageMetadata.ROTATION_0;
+            case 90:
+                return FirebaseVisionImageMetadata.ROTATION_90;
+            case 180:
+                return FirebaseVisionImageMetadata.ROTATION_180;
+            case 270:
+                return FirebaseVisionImageMetadata.ROTATION_270;
+            default:
+                throw new IllegalArgumentException(
+                        "Rotation must be 0, 90, 180, or 270.");
+        }
+    }
+
+    private void DrawFocusRect(int color) {
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        int height = mPreviewView.getHeight();
+        int width = mPreviewView.getWidth();
+
+        cameraHeight = height;
+        cameraWidth = width;
+
+        int left, right, top, bottom, diameter;
+
+        diameter = width;
+        if (height < width) {
+            diameter = height;
+        }
+
+        int offset = (int) (0.05 * diameter);
+        diameter -= offset;
+
+        canvas = holder.lockCanvas();
+        canvas.drawColor(0, PorterDuff.Mode.CLEAR);
+        //border's properties
+        paint = new Paint();
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(color);
+        paint.setStrokeWidth(5);
+
+        left = width / 2 - diameter / 3;
+        top = height / 2 - diameter / 3;
+        right = width / 2 + diameter / 3;
+        bottom = height / 2 + diameter / 3;
+
+        xOffset = left;
+        yOffset = top;
+        boxHeight = bottom - top;
+        boxWidth = right - left;
+        //Changing the value of x in diameter/x will change the size of the box ; inversely proportionate to x
+        canvas.drawRect(left, top, right, bottom, paint);
+        holder.unlockCanvasAndPost(canvas);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        //Drawing rectangle
+        DrawFocusRect(Color.parseColor("#b3dabb"));
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+
+    private boolean allPermissionsGranted(){
+
+        for(String permission : REQUIRED_PERMISSIONS){
+            if(ContextCompat.checkSelfPermission(getApplication(), permission) != PackageManager.PERMISSION_GRANTED){
+                return false;
+            }
+        }
+        return true;
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if(requestCode == REQUEST_CODE_PERMISSIONS){
+            if(allPermissionsGranted()){
+                startCamera();
+            } else{
+                Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show();
+                this.finish();
+            }
+        }
+    }
+
+    private void startCamera() {
+
+        mPreviewView = findViewById(R.id.previewView);
+
+        final ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+
+        cameraProviderFuture.addListener(new Runnable() {
+            @Override
+            public void run() {
+                try {
+
+                    ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                    bindPreview(cameraProvider);
+
+                } catch (ExecutionException | InterruptedException e) {
+                    // No errors need to be handled for this Future.
+                    // This should never be reached.
+                }
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+
+        Preview preview = new Preview.Builder()
+                .build();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
+                .setTargetResolution(new Size(720, 1488))
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build();
+
+        imageAnalysis.setAnalyzer(executor, new ImageAnalysis.Analyzer() {
+            @SuppressLint("UnsafeExperimentalUsageError")
+            @Override
+            public void analyze(@NonNull ImageProxy image) {
+                int rotationDegrees = degreesToFirebaseRotation(image.getImageInfo().getRotationDegrees());
+                if (image == null || image.getImage() == null) {
+                    return;
+                }
+
+                final Image mediaImage = image.getImage();
+                FirebaseVisionImage images = FirebaseVisionImage.fromMediaImage(mediaImage, rotationDegrees);
+                Bitmap bmp = images.getBitmap();
+                DisplayMetrics displaymetrics = new DisplayMetrics();
+                getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+
+                int height = bmp.getHeight();
+                int width = bmp.getWidth();
+
+                int left, right, top, bottom, diameter;
+
+                diameter = width;
+                if (height < width) {
+                    diameter = height;
+                }
+
+                int offset = (int) (0.05 * diameter);
+                diameter -= offset;
+
+
+                left = width / 2 - diameter / 3;
+                top = height / 2 - diameter / 3;
+                right = width / 2 + diameter / 3;
+                bottom = height / 2 + diameter / 3;
+
+                xOffset = left;
+                yOffset = top;
+
+                Bitmap bitmap = Bitmap.createBitmap(bmp, left, top, boxWidth, boxHeight);
+
+                FirebaseVisionCloudTextRecognizerOptions options =
+                        new FirebaseVisionCloudTextRecognizerOptions.Builder()
+                                .setLanguageHints(Arrays.asList("ru", "en"))
+                                .build();
+                FirebaseVisionTextRecognizer textRecognizer = FirebaseVision.getInstance()
+                        .getCloudTextRecognizer(options);
+
+                textRecognizer.processImage(FirebaseVisionImage.fromBitmap(bitmap))
+                        .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+                            @Override
+                            public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                                textView = findViewById(R.id.text);
+                                String text = firebaseVisionText.getText();
+                                textView.setText(text);
+
+                                for (FirebaseVisionText.TextBlock block : firebaseVisionText.getTextBlocks()) {
+                                    String blockText = block.getText();
+                                    for (FirebaseVisionText.Line line : block.getLines()) {
+                                        String lineText = line.getText();
+                                        for (FirebaseVisionText.Element element : line.getElements()) {
+                                            String elementText = element.getText();
+                                        }
+                                    }
+                                }
+                                image.close();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.e("Error", e.toString());
+                                image.close();
+                            }
+                        });
+            }
+        });
+        //ImageCapture.Builder builder = new ImageCapture.Builder();
+
+        //Vendor-Extensions (The CameraX extensions dependency in build.gradle)
+        //HdrImageCaptureExtender hdrImageCaptureExtender = HdrImageCaptureExtender.create(builder);
+
+        // Query if extension is available (optional).
+        //if (hdrImageCaptureExtender.isExtensionAvailable(cameraSelector)) {
+            // Enable the extension if available.
+        //    hdrImageCaptureExtender.enableExtension(cameraSelector);
+        //}
+
+        //final ImageCapture imageCapture = builder
+        //        .setTargetRotation(this.getWindowManager().getDefaultDisplay().getRotation())
+        //        .build();
+        preview.setSurfaceProvider(mPreviewView.createSurfaceProvider());
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, imageAnalysis, preview);
+
+
+
+//        captureImage.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                SimpleDateFormat mDateFormat = new SimpleDateFormat("yyyyMMddHHmmss", Locale.US);
+//                File file = new File(getBatchDirectoryName(), mDateFormat.format(new Date())+ ".jpg");
+//
+//                ImageCapture.OutputFileOptions outputFileOptions = new ImageCapture.OutputFileOptions.Builder(file).build();
+//                imageCapture.takePicture(outputFileOptions, executor, new ImageCapture.OnImageSavedCallback () {
+//                    @Override
+//                    public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+//                        new Handler().post(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                Toast.makeText(MainActivity.this, "Image Saved successfully", Toast.LENGTH_SHORT).show();
+//                            }
+//                        });
+//                    }
+//                    @Override
+//                    public void onError(@NonNull ImageCaptureException error) {
+//                        error.printStackTrace();
+//                    }
+//                });
+//            }
+//        });
+    }
+
+    public String getBatchDirectoryName() {
+
+        String app_folder_path = "";
+        app_folder_path = Environment.getExternalStorageDirectory().toString() + "/images";
+        File dir = new File(app_folder_path);
+        if (!dir.exists() && !dir.mkdirs()) {
+
+        }
+
+        return app_folder_path;
     }
 }
